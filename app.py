14ebfc99be269ac3412784ff541faa8592f4ebbd
Alican Yüksel <alicanyuksel@outlook.com>
2020-12-20 18:40:36 +0300

    Added the config file for the deployment

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_name}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 api.add_resource(Earthquake, "/earthquake/<int:id_>")
 api.add_resource(EarthquakesList, "/earthquakes")
 
@@ -30,5 +24,4 @@ def index():
 
 
 if __name__ == "__main__":
-    db.init_app(app)
     app.run(port=5000, debug=True)

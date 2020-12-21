3624421e50f94bf1ef8ab267fa501a7999059fbc
Alican Yüksel <alicanyuksel@outlook.com>
2020-12-21 03:05:24 +0300

    some error fixed and added a db file for localhost

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-domain_name = #CHANGE IT
+domain_name = ""  # CHANGE IT
+
+db_name = "data.db" # To work on localhost !!
 data_txt_name = "data.txt"
 
 url_api_backend = f"{domain_name}/earthquakes"

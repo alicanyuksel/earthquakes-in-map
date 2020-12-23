c17ba5fb4c56a9fc0ecb003c85a64c31f68a1026
Alican Yüksel <alicanyuksel@outlook.com>
2020-12-23 23:30:58 +0300

    Added a private key

--- static/js/map.js
+++ static/js/map.js
@@ -125,7 +125,7 @@ listFeaturesImportantQuakes = getImportantQuakes(myGeocode, listFeaturesImportan
 // call function to initialize the map with the features as parameters.
 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpY2FueXVrc2VsIiwiYSI6ImNrZjdqeXRoYzAzMzkzMGxjaHp4dzV3MXcifQ.R9_umqd9SXl02sJE8wQc3g';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpY2FueXVrc2VsIiwiYSI6ImNrajF1eHNmZDUwb3cycnFqbmh0ZnUwankifQ.4qDM0Cs441wYsRUyCrsM4Q';
 var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/alicanyuksel/ckf7kg2t70iah19np73mhnx48', // stylesheet location

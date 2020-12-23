3149fad6ddf9d3900f9bb56bb3c1ea60eaceb900
Alican Yüksel <alicanyuksel@outlook.com>
2020-12-23 18:47:55 +0300

    Added a function which display a description popup when users click on any quake in the righ side menu

--- static/js/importantQuake.js
+++ static/js/importantQuake.js
@@ -8,8 +8,16 @@ function writeImportantQuakes(listFeaturesImportantQuakes, rangeValue) {
     else {
         let htmlContent = "<center><h5>Önemli depremler</h5></center><hr>"
         for (var key in listFeaturesImportantQuakes) {
+
+            let coordinates = [parseFloat(listFeaturesImportantQuakes[key].longitude), parseFloat(listFeaturesImportantQuakes[key].latitude)];
+            let informationPopup = `<strong>Lokasyon : ${listFeaturesImportantQuakes[key].location}</strong><br>\
+                                    Şiddet : ${listFeaturesImportantQuakes[key].magnitude}<br>\
+                                    Derinlik : ${listFeaturesImportantQuakes[key].depth}<br>\
+                                    Tarih : ${listFeaturesImportantQuakes[key].time} - ${listFeaturesImportantQuakes[key].date}`
+
+
             htmlContent += `
-            <a id="linkToFly" href="#" onclick="geoToFly(${listFeaturesImportantQuakes[key].longitude}, ${listFeaturesImportantQuakes[key].latitude}); toggleNav();"><p>Lokasyon: ${listFeaturesImportantQuakes[key].location}</p></a>
+            <a id="linkToFly" href="#" onclick="geoToFly(${listFeaturesImportantQuakes[key].longitude}, ${listFeaturesImportantQuakes[key].latitude}); toggleNav(); displayPopup([${coordinates}], '${informationPopup}');"><p>Lokasyon: ${listFeaturesImportantQuakes[key].location}</p></a>
             <b><p style=color:red;>Şiddet: ${listFeaturesImportantQuakes[key].magnitude}</p></b>
             <p>Derinlik: ${listFeaturesImportantQuakes[key].depth}</p>
             <p>Saat/Tarih: ${listFeaturesImportantQuakes[key].time} - ${listFeaturesImportantQuakes[key].date}</p><hr>
@@ -20,7 +28,6 @@ function writeImportantQuakes(listFeaturesImportantQuakes, rangeValue) {
         
     }
 }
-
 function geoToFly(longitude, latitude) {
     map.flyTo(
         {
@@ -28,4 +35,15 @@ function geoToFly(longitude, latitude) {
             zoom: 8
         }
     )
+};
+
+// to display a popup when users click on the important quake in the right side menu
+function displayPopup(coordinates, description) {
+    
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: true
+    });
+
+    return popup.setLngLat(coordinates).setHTML(description).addTo(map);
 };
--- static/js/lastQuakes.js
+++ static/js/lastQuakes.js
@@ -2,24 +2,31 @@ function writeLastQuakes(detailsOfAllQuakes, rangeValue) {
 
     let htmlContent = `<center><h5>Son ${rangeValue} Depremler</h5></center><hr>`
     for (var key in detailsOfAllQuakes.slice(null, rangeValue)) {
+        
+        let coordinates = [parseFloat(detailsOfAllQuakes[key].longitude), parseFloat(detailsOfAllQuakes[key].latitude)];
+        let informationPopup = `<strong>Lokasyon : ${detailsOfAllQuakes[key].location}</strong><br>\
+                                Şiddet : ${detailsOfAllQuakes[key].magnitude}<br>\
+                                Derinlik : ${detailsOfAllQuakes[key].depth}<br>\
+                                Tarih : ${detailsOfAllQuakes[key].time} - ${detailsOfAllQuakes[key].date}`
+        
 
         if (detailsOfAllQuakes[key].magnitude > 0 && detailsOfAllQuakes[key].magnitude <= 2) {
             htmlContent += `
-            <a id="linkToFly" href="#" onclick="geoToFly(${detailsOfAllQuakes[key].longitude}, ${detailsOfAllQuakes[key].latitude}); toggleNav();"><p>Lokasyon: ${detailsOfAllQuakes[key].location}</p></a>
+            <a id="linkToFly" href="#" onclick="geoToFly(${detailsOfAllQuakes[key].longitude}, ${detailsOfAllQuakes[key].latitude}); toggleNav(); displayPopup([${coordinates}], '${informationPopup}');"><p>Lokasyon: ${detailsOfAllQuakes[key].location}</p></a>
             <b><p style=color:#4daf7c;>Şiddet: ${detailsOfAllQuakes[key].magnitude}</p></b>
             <p>Derinlik: ${detailsOfAllQuakes[key].depth}</p>
             <p>Saat/Tarih: ${detailsOfAllQuakes[key].time} - ${detailsOfAllQuakes[key].date}</p><hr>`
         }
         else if (detailsOfAllQuakes[key].magnitude >= 2 && detailsOfAllQuakes[key].magnitude <= 4) {
             htmlContent += `
-            <a id="linkToFly" href="#" onclick="geoToFly(${detailsOfAllQuakes[key].longitude}, ${detailsOfAllQuakes[key].latitude}); toggleNav();"><p>Lokasyon: ${detailsOfAllQuakes[key].location}</p></a>
+            <a id="linkToFly" href="#" onclick="geoToFly(${detailsOfAllQuakes[key].longitude}, ${detailsOfAllQuakes[key].latitude}); toggleNav(); displayPopup([${coordinates}], '${informationPopup}');"><p>Lokasyon: ${detailsOfAllQuakes[key].location}</p></a>
             <b><p style=color:#F7CA18;>Şiddet: ${detailsOfAllQuakes[key].magnitude}</p></b>
             <p>Derinlik: ${detailsOfAllQuakes[key].depth}</p>
             <p>Saat/Tarih: ${detailsOfAllQuakes[key].time} - ${detailsOfAllQuakes[key].date}</p><hr>`
         }
         else if (detailsOfAllQuakes[key].magnitude >= 4) {
             htmlContent += `
-            <a id="linkToFly" href="#" onclick="geoToFly(${detailsOfAllQuakes[key].longitude}, ${detailsOfAllQuakes[key].latitude}); toggleNav();"><p>Lokasyon: ${detailsOfAllQuakes[key].location}</p></a>
+            <a id="linkToFly" href="#" onclick="geoToFly(${detailsOfAllQuakes[key].longitude}, ${detailsOfAllQuakes[key].latitude}); toggleNav(); displayPopup([${coordinates}], '${informationPopup}');"><p>Lokasyon: ${detailsOfAllQuakes[key].location}</p></a>
             <b><p style=color:red;>Şiddet: ${detailsOfAllQuakes[key].magnitude}</p></b>
             <p>Derinlik: ${detailsOfAllQuakes[key].depth}</p>
             <p>Saat/Tarih: ${detailsOfAllQuakes[key].time} - ${detailsOfAllQuakes[key].date}</p><hr>`
@@ -37,4 +44,15 @@ function geoToFly(longitude, latitude) {
             zoom: 8
         }
     )
+};
+
+// to display a popup when users click on any quake in the right side menu
+function displayPopup(coordinates, description) {
+    
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: true
+    });
+
+    return popup.setLngLat(coordinates).setHTML(description).addTo(map);
 };
